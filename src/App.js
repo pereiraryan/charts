ab161f59882eb75ce929ee025c8073ab34c13a45
TechTest <pereiraryan.rp@gmail.com>
2024-11-19 18:27:41 +0530

    inital setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Circle from "./component/Circle";
+import Lines from "./component/Lines";
+import ComposedBar from "./component/ComposedBar";
+import HighLight from "./component/HighLight";
+import SmoothLine from "./component/SmoothLine";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-[#F3F4FF]">
+      <h1 className="font-bold text-[32px] mb-10">Welcome to mypulse</h1>
+      <div className="grid grid-cols-5">
+        <div>
+          <div className="font-bold text-[22px] center ">
+            Chart title goes here
+          </div>
+          <div className="center text-[16px]">15 April - 21 April</div>
+          <Lines />
+        </div>
+        <div>
+          <div className="font-bold text-[22px] center">Chart title</div>
+          <Circle />
+        </div>
+        <div>
+          <div className="font-bold text-[22px] center">Chart title goes here</div>
+          <div>15 April - 21 April</div>
+
+          <HighLight />
+        </div>
+        <div>
+          <div className="font-bold text-[22px] center">Chart title goes here</div>
+          <div>15 April - 21 April</div>
+
+          <ComposedBar />
+        </div>
+        <div>
+        <div className="font-bold text-[22px] center">Chart title goes here</div>
+        <div>15 April - 21 April</div>
+        <SmoothLine />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/component/Lines.js
@@ -0,0 +1,89 @@
+import React from "react";
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+} from "recharts";
+
+const data = [
+  {
+    name: "Page A",
+
+    pv: 240,
+    amt: 240,
+  },
+  {
+    name: "Page B",
+
+    pv: 139,
+    amt: 220,
+  },
+  {
+    name: "Page C",
+
+    pv: 600,
+    amt: 290,
+  },
+  {
+    name: "Page D",
+
+    pv: 300,
+    amt: 200,
+  },
+  {
+    name: "Page E",
+
+    pv: 480,
+    amt: 281,
+  },
+  {
+    name: "Page F",
+
+    pv: 380,
+    amt: 250,
+  },
+  {
+    name: "Page G",
+
+    pv: 430,
+    amt: 210,
+  },
+];
+
+export default function Lines() {
+  return (
+    <AreaChart
+      width={250}
+      height={100}
+      data={data}
+      margin={{
+        top: 10,
+        right: 30,
+        left: 0,
+        bottom: 0,
+      }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+      <Area
+        type="monotone"
+        dataKey="pv"
+        stackId="1"
+        stroke="#147AD6"
+        fill="#147AD6"
+      />
+      <Area
+        type="monotone"
+        dataKey="amt"
+        stackId="1"
+        stroke="#EC6666"
+        fill="#EC6666"
+      />
+    </AreaChart>
+  );
+}
