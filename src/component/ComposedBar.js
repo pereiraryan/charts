c41372fe29c83ddd2ea42b08f1f114219835201e
TechTest <pereiraryan.rp@gmail.com>
2024-11-19 19:26:53 +0530

    charts up to design v3

--- src/component/ComposedBar.js
+++ src/component/ComposedBar.js
@@ -2,57 +2,55 @@ import React from "react";
 import {
   ComposedChart,
   Line,
-  Area,
   Bar,
   XAxis,
   YAxis,
   CartesianGrid,
   Tooltip,
   Legend,
-  Scatter
 } from "recharts";
 
 const data = [
   {
-    name: "Page A",
-    uv: 590,
-    pv: 800,
-    amt: 1400,
+    name: "JAN",
+    uv: 500,
+    pv: 500,
+    amt: 140,
     cnt: 490
   },
   {
-    name: "Page B",
-    uv: 868,
-    pv: 967,
-    amt: 1506,
+    name: "FEB",
+    uv: 268,
+    pv: 500,
+    amt: 150,
     cnt: 590
   },
   {
-    name: "Page C",
-    uv: 1397,
-    pv: 1098,
-    amt: 989,
+    name: "MAR",
+    uv: 397,
+    pv: 500,
+    amt: 289,
     cnt: 350
   },
   {
-    name: "Page D",
-    uv: 1480,
-    pv: 1200,
-    amt: 1228,
+    name: "APR",
+    uv: 180,
+    pv: 500,
+    amt: 122,
     cnt: 480
   },
   {
-    name: "Page E",
-    uv: 1520,
-    pv: 1108,
-    amt: 1100,
+    name: "MAY",
+    uv: 120,
+    pv: 500,
+    amt: 110,
     cnt: 460
   },
   {
-    name: "Page F",
-    uv: 1400,
-    pv: 680,
-    amt: 1700,
+    name: "JUN",
+    uv: 400,
+    pv: 500,
+    amt: 170,
     cnt: 380
   }
 ];
@@ -75,10 +73,8 @@ export default function ComposedBar() {
       <YAxis />
       <Tooltip />
       <Legend />
-      <Area type="monotone" dataKey="amt" fill="#8884d8" stroke="#8884d8" />
-      <Bar dataKey="pv" barSize={20} fill="#413ea0" />
+      <Bar dataKey="pv" barSize={20} fill="#F5F6FA" />
       <Line type="monotone" dataKey="uv" stroke="#ff7300" />
-      <Scatter dataKey="cnt" fill="red" />
     </ComposedChart>
   );
 }
